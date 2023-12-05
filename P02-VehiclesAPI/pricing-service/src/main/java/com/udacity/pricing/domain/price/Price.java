c3c5d7c93572348e8b9d9d02a90df7f37608e100
dongtang <tangbinhdong@gmail.com>
2023-12-04 23:44:34 -0600

    Update to java 17 and spring boot 3 to compatible with eureka server

--- P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ P02-VehiclesAPI/pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,7 +1,9 @@
 package com.udacity.pricing.domain.price;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+
 import java.math.BigDecimal;
 
 /**
